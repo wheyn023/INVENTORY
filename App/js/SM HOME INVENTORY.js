33c4d35032583eeda97b69e146163127e74a6557
wheyn023 <163097810+wheyn023@users.noreply.github.com>
2024-03-12 14:31:38 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/SM HOME INVENTORY.js
+++ App/js/SM HOME INVENTORY.js
@@ -1,16 +1,47 @@
-xui.Class('App.SM HOME INVENTORY', 'xui.Module',{
+// The default code is a module class (inherited from xui.Module)
+// Ensure that all the value of "key/value pair" does not refer to external variables
+xui.Class('AppSMHOME', 'xui.Module',{
     Instance:{
+        // Dependency classes
+        Dependencies:[],
+        // Required modules
+        Required:[],
+
+        // To initialize properties
+        properties : {},
+
+        // To initialize instance(e.g. properties)
+        initialize : function(){
+        },
+
+        // To initialize internal components (mostly UI controls)
+        // *** If you're not a skilled, dont modify this function manually ***
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-            
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
-            );
-            
+
             return children;
             // ]]Code created by CrossUI RAD Studio
+        },
+
+        // Give a chance to determine which UI controls will be appended to parent container
+        customAppend : function(parent, subId, left, top){
+            // "return false" will cause all the internal UI controls will be added to the parent panel
+            return false;
         }
+        /*,
+        // To determine how properties affects this module
+        propSetAction : function(prop){
+        },
+        // To set all node's style in this modlue
+        customStyle:{}
+    },
+    //To customize the default properties and event handlers
+    Static:{
+        $DataModel:{
+        },
+        $EventHandlers:{
+        }
+    */
     }
 });
