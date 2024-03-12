c5874992aa8b6918485c36ac1918ad37e0f928ea
wheyn023 <163097810+wheyn023@users.noreply.github.com>
2024-03-12 14:31:28 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,93 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button7")
+                .setLeft("11.428571428571429em")
+                .setTop("6.095238095238095em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("SM HOME")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "page",
+                        "target" : "App.SM HOME INVENTORY",
+                        "args" : [
+                            {
+                            }
+                        ],
+                        "method" : "open",
+                        "event" : 1
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button11")
+                .setLeft("22.095238095238095em")
+                .setTop("6.095238095238095em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("SURPLUS")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "page",
+                        "target" : "App.SURPLUS INVENTORY",
+                        "args" : [
+                            {
+                            }
+                        ],
+                        "method" : "open",
+                        "event" : 1
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button12")
+                .setLeft("11.428571428571429em")
+                .setTop("11.428571428571429em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("UPTREND")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button13")
+                .setLeft("22.095238095238095em")
+                .setTop("11.428571428571429em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("NURSERY")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button14")
+                .setLeft("11.428571428571429em")
+                .setTop("16.761904761904763em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("WALTER OR")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button15")
+                .setLeft("22.095238095238095em")
+                .setTop("16.761904761904763em")
+                .setWidth("9.82857142857143em")
+                .setHeight("4.3428571428571425em")
+                .setCaption("WHSE INVENTORY")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
